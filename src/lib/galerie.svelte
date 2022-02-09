605e443e546ffb34ce6756e1b3eeccfe99529a2b
Louis T <67950802+LouisT2469@users.noreply.github.com>
2022-02-09 18:26:35 +0100

    add Galerie
    add "galerie.json"
    add "galerie.svelte"
    add "galerie.svelte"
    add "galerie" folder

---
+++ src/lib/galerie.svelte
@@ -0,0 +1,29 @@
+<script>
+import data from './JSON/galerie.json';
+</script>
+
+
+<section class="container p-6 mx-auto">
+	<h2 class="text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl">My Designs:</h2>
+    {#each data.Bilder as item}
+    <div class="">
+    <div class="flex items-center justify-center ">
+            <div class="grid gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                <div class="w-full max-w-xs text-center border-gray-200 border p-4 rounded-lg backdrop-blur-xl">
+                    <img
+                        class="object-cover object-center w-full h-48 mx-auto"
+                        src="/galerie/{item.source}"
+                        alt="{item.alt}"
+                    />
+    
+                    <div class="mt-2 border-gray-200 border rounded-lg">
+                        <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">{item.subtile}</h3>
+                        <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">--by {item.creator}--</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {/each}
+	
+</section>
---
+++ src/routes/galerie.svelte
@@ -0,0 +1,22 @@
+<script>
+import Navbar from "$lib/navbar.svelte";
+import Footer from "$lib/footer.svelte";
+import Galerie from "$lib/galerie.svelte";
+</script>
+
+<svelte:head>
+    <title>Galerie | louis T.</title>
+    </svelte:head>
+    <Navbar />
+
+    <Galerie/>
+
+        <Footer/>
+
+<style>
+:global(body) {
+    background: url('/hintergrund.svg');
+    background-size: cover;
+    background-position: center;
+}
+</style>
