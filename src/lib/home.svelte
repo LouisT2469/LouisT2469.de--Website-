43acb90e6b9de6522ffc79e1f843f06f9fdda0e0
Louis T <67950802+LouisT2469@users.noreply.github.com>
2022-02-10 20:59:37 +0100

    edit galerie
    add galerie to the index.html

---
+++ src/lib/home.svelte
@@ -0,0 +1,31 @@
+<script>
+	import data from './JSON/projekte.json';
+</script>
+
+<section class="container p-6 mx-auto">
+	<h2 class="text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl m-4">
+		Meine Projekte:
+	</h2>
+
+	<div class="m-8 content-start flex flex-wrap">
+
+		{#each data.Projekte as item}
+        <div class="flex place-items-center m-4 self-center">
+			<div
+				class="w-full max-w-xs text-center border-gray-200 border p-4 rounded-lg backdrop-blur-xl grid place-items-center"
+			>
+				<img
+					class="object-cover object-center w-full h-48 mx-auto"
+					src="/galerie/{item.Bild}"
+					alt="lll"
+				/>
+
+				<div class="mt-2 border-gray-200 border rounded-lg">
+					<h3 class="text-lg font-medium text-gray-700 dark:text-gray-200 flex flex-wrap px-2">{item.text}</h3>
+					<a class="mt-1 font-medium text-blue-400" href="{item.src}">--click me--</a>
+				</div>
+			</div>
+		</div>
+        {/each}
+	</div>
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,21 @@
 <script>
-	import Navbar from '$lib/navbar.svelte';
-    import Footer from '$lib/footer.svelte';
+import Navbar from '$lib/navbar.svelte';
+import Footer from '$lib/footer.svelte';
+import Home from '$lib/home.svelte';
 </script>
 
 <svelte:head>
-	<title>Home | louis T.</title>
-</svelte:head>
-<Navbar />
+    <title>Home | louis T.</title>
+    </svelte:head>
+    <Navbar />
+    <Home/>
 
+        <Footer/>
 
-<Footer/>
 <style>
-	:global(body) {
-		background: url('/hintergrund.svg');
-		background-size: cover;
-		background-position: center;
-	}
+:global(body) {
+    background: url('/hintergrund.svg');
+    background-size: cover;
+    background-position: center;
+}
 </style>
-
