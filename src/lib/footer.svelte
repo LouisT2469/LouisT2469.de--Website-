a8651bdbbbc9575754869bcfcac524c54d51c1d0
Louis T <67950802+LouisT2469@users.noreply.github.com>
2022-02-21 16:14:36 +0100

    V. 3

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,27 +1,43 @@
 <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900" href="/">
-        <img src="/favicon.png" alt="Logo" class="w-12 h-12 text-white p-2">
-        <span class="ml-3 text-xl text-white">Louis T. |</span>
-    </a>
-    <a class="ml-3 text-white" href="./impressum.html">
-        <span class="ml-5 text-lg text-white">Impressum</span>
-    </a>
+	<a
+		class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+		href="/"
+	>
+		<img src="/favicon.png" alt="Logo" class="w-12 h-12 text-white p-2" />
+		<span class="ml-3 text-xl text-white">Louis T. |</span>
+	</a>
+	<a class="ml-3 text-white" href="./impressum.html">
+		<span class="ml-5 text-lg text-white">Impressum</span>
+	</a>
 
-    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-        <a class="ml-3 text-white" href="https://twitter.com/LouisT44710854">
-            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                class="w-5 h-5" viewBox="0 0 24 24">
-                <path
-                    d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
-                </path>
-            </svg>
-        </a>
-        <a class="ml-3 text-white" href="https://www.instagram.com/louist2469/">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-            </svg>
-        </a>
-    </span>
+	<span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+		<a class="ml-3 text-white" href="https://twitter.com/LouisT44710854">
+			<svg
+				fill="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				class="w-5 h-5"
+				viewBox="0 0 24 24"
+			>
+				<path
+					d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
+				/>
+			</svg>
+		</a>
+		<a class="ml-3 text-white" href="https://www.instagram.com/louist2469/">
+			<svg
+				fill="none"
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				class="w-5 h-5"
+				viewBox="0 0 24 24"
+			>
+				<rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+				<path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+			</svg>
+		</a>
+	</span>
 </div>
