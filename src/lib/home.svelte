20afbb0930c619eaf69b771326f3e97e49373003
Louis T <67950802+LouisT2469@users.noreply.github.com>
2022-02-21 11:27:22 +0100

    edit design of galerie
    edit the design of the project preview "galerie"

--- src/lib/home.svelte
+++ src/lib/home.svelte
@@ -1,31 +1,28 @@
 <script>
-	import data from './JSON/projekte.json';
+import data from './JSON/projekte.json';
 </script>
 
 <section class="container p-6 mx-auto">
-	<h2 class="text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl m-4">
-		Meine Projekte:
-	</h2>
+    <h2 class="text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl m-4">
+        Meine Projekte:
+    </h2>
 
-	<div class="m-8 content-start flex flex-wrap">
+    <div class="m-8 content-start flex flex-wrap">
 
-		{#each data.Projekte as item}
+        {#each data.Projekte as item}
         <div class="flex place-items-center m-4 self-center">
-			<div
-				class="w-full max-w-xs text-center border-gray-200 border p-4 rounded-lg backdrop-blur-xl grid place-items-center"
-			>
-				<img
-					class="object-cover object-center w-full h-48 mx-auto"
-					src="/projekte/{item.Bild}"
-					alt="project"
-				/>
-
-				<div class="mt-2 border-gray-200 border rounded-lg">
-					<h3 class="text-lg font-medium text-gray-700 dark:text-gray-200 flex flex-wrap px-2">{item.text}</h3>
-					<a class="mt-1 font-medium text-blue-400" href="{item.src}">--click me--</a>
-				</div>
-			</div>
-		</div>
+            <div
+                class="w-full max-w-xs text-center border-gray-200 border p-4 rounded-lg backdrop-blur-xl grid place-items-center"
+                >
+                <a href="{item.src}">
+                    <img
+                        class="object-cover object-center w-full h-48 mx-auto m-2 hover:blur-sm"
+                        src="/projekte/{item.Bild}"
+                        alt="project"
+                        />
+                </a>
+            </div>
+        </div>
         {/each}
-	</div>
+    </div>
 </section>
