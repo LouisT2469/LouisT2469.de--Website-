3c46c83afee0feee0886843d3a7c8c2070fd95a3
Louis T <67950802+LouisT2469@users.noreply.github.com>
2022-02-25 00:54:57 +0100

    sonderzeichen v 1.2

--- src/lib/sonderzeichen.svelte
+++ src/lib/sonderzeichen.svelte
@@ -10,7 +10,7 @@ import data from './JSON/sonderzeichen.json'
                 <div class="mt-5">
                     <div class="font-semibold uppercase text-center p-2 bg-purple-800 rounded-md h-10 grid">{item.Headname}</div>
                     {#each item.Content as subitem}
-                    <div class="text-center p-2 bg-purple-500 hover:bg-purple-400 hover:scale-105 hover:border-2 hover:border-purple-200 duration-200 h-16 mt-2 rounded-md grid place-items-center cursor-pointer">{subitem}</div>
+                    <div class="text-center p-2 bg-purple-500 hover:bg-purple-400 hover:scale-105 hover:border-2 hover:border-purple-200 duration-200 h-16 mt-2 rounded-md grid place-items-center cursor-pointer select-all">{subitem}</div>
                     {/each}
                 </div>
                 {/each}
